1988168034d460f4a65ebc9fff47c96befebdd53
BalaVarun123 <lsv.mail.id@gmail.com>
2021-04-01 19:51:00 +0530

    Initial commit

---
+++ SpringGradleGitTest/src/main/java/com/me/sggt/SpringGradleGitTestApplication.java
@@ -0,0 +1,13 @@
+package com.me.sggt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleGitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradleGitTestApplication.class, args);
+	}
+
+}
---
+++ SpringGradleGitTest/src/test/java/com/me/sggt/SpringGradleGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.sggt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleGitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
